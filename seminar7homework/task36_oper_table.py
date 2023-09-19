ef7c7cb9d414138140075f0434534ce061719a74
KirillGCPD <akadelpher@gmail.com>
2023-09-19 12:30:15 +0800

    lambda table

---
+++ seminar7homework/task36_oper_table.py
@@ -0,0 +1,22 @@
+"""Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны. 
+Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+*Пример:*
+
+**Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+**Вывод:**"""
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for x in range(1,num_rows+1):
+        for y in range(1,num_columns+1):
+            print(f"{operation(x,y)}",end="\t")
+        print(end="\n")
+
+print(f"Операция умножения *: ")
+print_operation_table(lambda x, y: x * y)
+print(f"Нарисовать треугольник из плсюв через лямпду")
+print_operation_table(lambda x, y: "+" if x >= y else "-")
